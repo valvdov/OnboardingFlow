5ae6bcb5527c16b193cbbd03c0d5a118dccca80b
Valerii <valvdov@gmail.com>
2025-05-17 19:00:13 +0100

    Finish

---
+++ OnboardingFlow/FeaturesPage.swift
@@ -0,0 +1,18 @@
+//
+//  FeaturesPage.swift
+//  OnboardingFlow
+//
+//  Created by Valera Vdovin on 17/05/2025.
+//
+
+import SwiftUI
+
+struct FeaturesPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeaturesPage()
+}
---
+++ OnboardingFlow/WelcomePage.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomePage.swift
+//  OnboardingFlow
+//
+//  Created by Valera Vdovin on 17/05/2025.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}
